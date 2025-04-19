14840e781b4f4a3e7115df53ea16f8e898695c42
Ewen <ewen_gallet@hotmail.com>
2025-04-20 01:09:23 +0200

    feat: Update ConnectionService to match CustomerService pattern

--- src/main/java/com/saltedge/connector/service/ConnectionService.java
+++ src/main/java/com/saltedge/connector/service/ConnectionService.java
@@ -1,8 +1,12 @@
 package com.saltedge.connector.service;
 
 import com.saltedge.connector.model.Connection;
+import com.saltedge.connector.model.response.SaltEdgeResponse;
+import com.saltedge.connector.model.response.SaltEdgeSingleResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -17,22 +21,32 @@ public class ConnectionService {
         this.saltEdgeService = saltEdgeService;
     }
 
-    public Mono<Connection> createConnection(String customerId, String providerCode, Map<String, Object> credentials) {
-        Map<String, Object> requestBody = Map.of(
-                "customer_id", customerId,
-                "provider_code", providerCode,
-                "credentials", credentials
-        );
-        return saltEdgeService.post("/connections", requestBody, Connection.class);
+    public Flux<Connection> getAllConnections() {
+        return saltEdgeService.get("/connections", new ParameterizedTypeReference<SaltEdgeResponse<Connection>>() {})
+                .flatMapMany(response -> Flux.fromIterable(response.getData()));
     }
 
     public Mono<Connection> getConnection(String connectionId) {
-        return saltEdgeService.get("/connections/" + connectionId, Connection.class);
+        return saltEdgeService.get("/connections/" + connectionId, new ParameterizedTypeReference<SaltEdgeSingleResponse<Connection>>() {})
+                .map(SaltEdgeSingleResponse::getData);
     }
 
-    public Mono<Connection> updateConnection(String connectionId, Map<String, Object> credentials) {
-        Map<String, Object> requestBody = Map.of("credentials", credentials);
-        return saltEdgeService.put("/connections/" + connectionId, requestBody, Connection.class);
+    public Mono<Connection> createConnection(String customerId, String providerCode, String countryCode) {
+        Map<String, Object> requestBody = Map.of(
+                "data", Map.of(
+                        "customer_id", customerId,
+                        "provider_code", providerCode,
+                        "country_code", countryCode
+                )
+        );
+        return saltEdgeService.post("/connections", requestBody, new ParameterizedTypeReference<SaltEdgeSingleResponse<Connection>>() {})
+                .map(SaltEdgeSingleResponse::getData);
+    }
+
+    public Mono<Connection> updateConnection(String connectionId, Map<String, Object> attributes) {
+        Map<String, Object> requestBody = Map.of("data", attributes);
+        return saltEdgeService.put("/connections/" + connectionId, requestBody, new ParameterizedTypeReference<SaltEdgeSingleResponse<Connection>>() {})
+                .map(SaltEdgeSingleResponse::getData);
     }
 
     public Mono<Void> deleteConnection(String connectionId) {
@@ -40,6 +54,12 @@ public class ConnectionService {
     }
 
     public Mono<Connection> refreshConnection(String connectionId) {
-        return saltEdgeService.put("/connections/" + connectionId + "/refresh", null, Connection.class);
+        return saltEdgeService.put("/connections/" + connectionId + "/refresh", Map.of(), new ParameterizedTypeReference<SaltEdgeSingleResponse<Connection>>() {})
+                .map(SaltEdgeSingleResponse::getData);
+    }
+
+    public Mono<Connection> reconnectConnection(String connectionId) {
+        return saltEdgeService.put("/connections/" + connectionId + "/reconnect", Map.of(), new ParameterizedTypeReference<SaltEdgeSingleResponse<Connection>>() {})
+                .map(SaltEdgeSingleResponse::getData);
     }
 } 
