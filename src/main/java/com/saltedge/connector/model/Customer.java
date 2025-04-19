45059ac52cdb50c04bdab0a3a3ba9b70f1823303
Ewen <ewen_gallet@hotmail.com>
2025-04-19 22:41:41 +0200

    feat: Update Customer model to match Salt Edge API response format

--- src/main/java/com/saltedge/connector/model/Customer.java
+++ src/main/java/com/saltedge/connector/model/Customer.java
@@ -3,69 +3,25 @@ package com.saltedge.connector.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Data
 public class Customer {
-    private String id;
+    @JsonProperty("customer_id")
+    private String customerId;
+
+    @JsonProperty("identifier")
     private String identifier;
-    private String secret;
-    private String status;
-    
+
+    @JsonProperty("categorization_type")
+    private String categorizationType;
+
+    @JsonProperty("blocked_at")
+    private Instant blockedAt;
+
     @JsonProperty("created_at")
-    private LocalDateTime createdAt;
-    
+    private Instant createdAt;
+
     @JsonProperty("updated_at")
-    private LocalDateTime updatedAt;
-    
-    @JsonProperty("last_accessed_at")
-    private LocalDateTime lastAccessedAt;
-    
-    @JsonProperty("consent_given_at")
-    private LocalDateTime consentGivenAt;
-    
-    @JsonProperty("consent_withdrawn_at")
-    private LocalDateTime consentWithdrawnAt;
-    
-    @JsonProperty("consent_expires_at")
-    private LocalDateTime consentExpiresAt;
-    
-    @JsonProperty("consent_required")
-    private boolean consentRequired;
-    
-    @JsonProperty("consent_given")
-    private boolean consentGiven;
-    
-    @JsonProperty("consent_withdrawn")
-    private boolean consentWithdrawn;
-    
-    @JsonProperty("consent_expired")
-    private boolean consentExpired;
-    
-    @JsonProperty("consent_valid_until")
-    private LocalDateTime consentValidUntil;
-    
-    @JsonProperty("consent_valid_for_days")
-    private Integer consentValidForDays;
-    
-    @JsonProperty("consent_scopes")
-    private String[] consentScopes;
-    
-    @JsonProperty("consent_shared_data")
-    private String[] consentSharedData;
-    
-    @JsonProperty("consent_shared_data_expires_at")
-    private LocalDateTime consentSharedDataExpiresAt;
-    
-    @JsonProperty("consent_shared_data_valid_until")
-    private LocalDateTime consentSharedDataValidUntil;
-    
-    @JsonProperty("consent_shared_data_valid_for_days")
-    private Integer consentSharedDataValidForDays;
-    
-    @JsonProperty("consent_shared_data_scopes")
-    private String[] consentSharedDataScopes;
-    
-    @JsonProperty("consent_shared_data_shared_data")
-    private String[] consentSharedDataSharedData;
+    private Instant updatedAt;
 } 
